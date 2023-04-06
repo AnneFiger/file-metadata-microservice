ca56c725fa5b1d1974bf0fad8792bcafaacf0b0d
AnneFiger <annefiger@gmail.com>
2023-04-06 17:04:35 +0100

    Forked initial FCC boilerplate repo and installed multer

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function(req, res){ //request url taken from action on form. Might need changing
 
+});
 
 
 const port = process.env.PORT || 3000;
